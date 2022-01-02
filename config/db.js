d4d703099e65e5e8fd1ea01daac9cf9ba6aea787
Ankit Petwal <ankitpetwal03@gmail.com>
2022-01-02 16:40:50 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+function connectDB() {
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const connection = mongoose.connection;
+      try{
+     connection.once('open', () => {
+        console.log('Database connected');
+    });
+    } catch(err){
+        console.log('Connection failed');
+    }
+}
+module.exports = connectDB;
